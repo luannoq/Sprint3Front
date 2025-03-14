5f389a1215ba82a8447d2a6ba827acfaa5ddbdab
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-14 11:55:42 -0300

    Alterando a page comercios locais

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return(
-        <footer className="bg-[#C7CDCF] text-white flex h-[3.25rem]  items-center gap-4 pl-4 flex border-t border-black">
+        <footer className="bg-[#C7CDCF] text-white flex h-[3.25rem] items-center gap-4 pl-4  border-t border-black">
             <h3 className="text-black">2024 Grupo Spectrum</h3>
 
             <a href="https://www.instagram.com/spectrumfiap/">
